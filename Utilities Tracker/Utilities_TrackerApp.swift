57182980d395c39c22931d7a075c44fafaf14a5b
jnnsrctr <99033602+jnnsrctr@users.noreply.github.com>
2023-02-05 11:59:57 +0100

    Initial Commit

---
+++ Utilities Tracker/Utilities_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Utilities_TrackerApp.swift
+//  Utilities Tracker
+//
+//  Created by Johannes Richter on 05.02.23.
+//
+
+import SwiftUI
+
+@main
+struct Utilities_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
